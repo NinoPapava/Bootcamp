bcc454d826b8a6a4d1ccb0c764b7d4b772b70131
NinoPapava <gv.papava@gmail.com>
2023-04-21 17:17:36 +0400

    convert Decimal To Binary

---
+++ DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //convert decimal to binary
+            Console.WriteLine("Decimal:");
+
+            int decimalNumber = int.Parse(Console.ReadLine());
+            int remainder;
+            string result = string.Empty;
+
+            while(decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder.ToString() + result;
+            }
+            Console.WriteLine("Binary : {0}", result);
+            Console.ReadLine();
+
+        }
+    }
+}
